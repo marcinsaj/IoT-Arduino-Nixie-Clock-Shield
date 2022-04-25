fbac696c6008eaf6d6ca8556fc3a8b8e4f90b1d2
Marcin Saj <marcin@nixietester.com>
2022-04-25 11:48:35 +0200

    update name

--- examples/IoT-IN12-Nixie-Clock/arduino_secrets.h
+++
@@ -1,6 +0,0 @@
-// If you decide to use the Arduino Cloud Code Editor
-// then this file should be omitted.
-// Use only with Arduino IDE offline software.
-
-#define SECRET_SSID "type here your wifi network name"
-#define SECRET_PASS "type here your wifi password"
